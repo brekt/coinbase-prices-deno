026648a549d89412388df0596d5cae1ac82d17c0
brekt <speakoftrees@gmail.com>
2020-05-17 21:03:26 -0400

    migrating to git bash pushing up temp work

--- mod.ts
+++ mod.ts
@@ -1,23 +1,109 @@
 // @ts-ignore
 import { readFileStrSync } from 'https://deno.land/std/fs/mod.ts';
 // @ts-ignore
-import { IPrice } from './types.ts';
+import { hmac, HMAC } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
+// @ts-ignore
+import { Hash, Price } from './types.ts';
 
-const apiRootPro = 'https://api-public.sandbox.pro.coinbase.com';
+const coinPairs: string[] = [
+    'BTC-USD',
+    'ETH-USD',
+    'LINK-USD',
+    'BAT-USD',
+    // 'GNT-USD', not supported?
+    // 'CVC-USD', not supported?
+    // OMG-USDC coming May 18th, 2020 
+];
 
 const apiRoot = 'https://api.coinbase.com/v2';
+const apiRootPro = 'https://api.pro.coinbase.com';
 
-const apiKey = readFileStrSync('./api-key.env'); // Make this file yourself and add your Coinbase API key to it.
+/**
+ * Add the following git ignored file (api-key.env) that contains only your Coinbase API key.
+ * Do not just paste your key in here because you'll risk committing it to GitHub,
+ * where someone will find it and potentially steal your actual money.
+ */
+const apiKey = readFileStrSync('./api-key.env');
 
-const getCoinPrice = async (currencyPair: string): Promise<IPrice> => {
-    const price = await (await fetch(`${apiRoot}/prices/${currencyPair}/buy`)).json();
+/**
+ * Get market price of a cryptocurrency.
+ */
+const getCoinPrice = async (currencyPair: string): Promise<Price | void> => {
+    try {
+        const price = await (await fetch(`${apiRoot}/prices/${currencyPair}/spot`)).json();
+
+        if (price.errors) {
+            throw new Error(`Error in getCoinPrice for ${currencyPair}: ${JSON.stringify(price.errors)}`);
+        }
     
-    return price;
+        return price;
+    } catch (err) {
+        console.error(err);
+
+        return void 0;
+    }
 };
-  
 
-(async() => {
-    const btcPrice = await getCoinPrice('BTC-USD');
+const getAllCurrencies = async (): Promise<any> => {
+    try {
+        const pairs = await (await fetch(`${apiRootPro}/currencies/`)).json();
 
-    console.log(btcPrice.data);
+        return pairs;
+    } catch (err) {
+        console.error(err);
+
+        return void 0;
+    }
+};
+
+const hitOracle = async(): Promise<any> => {
+    const url = `${apiRootPro}/oracle`;
+    const signedMessage = signMessage(url);
+    const fetchOptions = {
+        headers: {
+            'CB-ACCESS-KEY': apiKey,
+            'CB-ACCESS-SIGN': signedMessage
+        }
+    };
+
+    try {
+        const oracleResponse = await(await fetch(`${apiRootPro}/oracle`, fetchOptions)).json();
+
+        return oracleResponse;
+    } catch (err) {
+        console.error(err);
+
+        return void 0;
+    }
+}
+
+(async () => {
+
+    const pricePromises = coinPairs.map(pair => getCoinPrice(pair));
+
+    for await (const price of pricePromises) {
+        price && console.log(price);
+    }
+
 })();
+
+// const signMessage = (url: string, method = 'GET', body = ''): string => {
+//     const timestamp = Date.now() / 1000;
+//     const requestPath = '/oracle';
+//     const stringifiedBody = JSON.stringify(body);
+//     const prehash = timestamp + method + requestPath + stringifiedBody;
+//     const key = btoa(apiKey);
+
+//     const sha256Hmac = hmac('sha256', key, pre);
+
+//     return sha256Hmac.update(prehash).digest('base64');
+// }
+
+
+// (async () => {
+
+//     const oracleResponse = await hitOracle();
+
+//     console.log(oracleResponse);
+
+// })();
--- types.ts
+++ types.ts
@@ -1,9 +1,16 @@
-interface IPriceData {
+interface PriceData {
     base: string;
     currency: string;
     amount: string;
 }
 
-export interface IPrice {
-    data: IPriceData;
+export interface Price {
+    data: PriceData;
+}
+
+export interface Hash {
+    hashSize: number;
+    init(): Hash;
+    update(msg: string | Uint8Array, inputEncoding?: string): Hash;
+    digest(outputEncoding?: string): string | Uint8Array;
 }
