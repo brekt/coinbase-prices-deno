d4c07c33c28194f1ba7e833a2115f97565067c50
brekt <speakoftrees@gmail.com>
2020-05-18 23:52:49 -0400

    use coinpro.io api

--- mod.ts
+++ mod.ts
@@ -1,23 +1,91 @@
 // @ts-ignore
 import { readFileStrSync } from 'https://deno.land/std/fs/mod.ts';
 // @ts-ignore
-import { IPrice } from './types.ts';
+import { Asset, Price } from './types.ts';
 
-const apiRootPro = 'https://api-public.sandbox.pro.coinbase.com';
+const apiRoot = 'https://rest.coinapi.io/v1';
 
-const apiRoot = 'https://api.coinbase.com/v2';
 
-const apiKey = readFileStrSync('./api-key.env'); // Make this file yourself and add your Coinbase API key to it.
+/**
+ * Add the following git ignored file (coinapi.io-key.env) that contains only your Coinapi.io API key.
+ * Do not just paste your key in here because you'll risk committing it to GitHub,
+ * where someone will find it and potentially steal your actual money.
+ */
+const apiKey = readFileStrSync('./coinapi.io-api-key.env');
 
-const getCoinPrice = async (currencyPair: string): Promise<IPrice> => {
-    const price = await (await fetch(`${apiRoot}/prices/${currencyPair}/buy`)).json();
-    
-    return price;
+const headers = {
+    'Accept': 'application/json',
+    'X-CoinAPI-Key': apiKey
 };
-  
 
-(async() => {
-    const btcPrice = await getCoinPrice('BTC-USD');
+const getExchanges = async(): Promise<object | null> => {
+    const url = `${apiRoot}/exchanges`;
+    const fetchOptions = {
+        headers
+    };
 
-    console.log(btcPrice.data);
+    try {
+        const response = await(await fetch(url, fetchOptions)).json();
+
+        return response
+    } catch (err) {
+        console.error(err);
+
+        return null;
+    }
+}
+
+const getAssets = async (): Promise<Array<Asset>> => {
+    const url = `${apiRoot}/assets`;
+    const fetchOptions = {
+        headers
+    };
+
+    try {
+        const response = await(await fetch(url, fetchOptions)).json();
+
+        return response
+    } catch (err) {
+        console.error(err);
+
+        return [];
+    }
+}
+
+const getMyAssets = async (): Promise<Array<Asset>> => {
+    const myCoins: string[] = [
+        'BTC',
+        'ETH',
+        'BAT',
+        'LINK',
+        'GNT',
+        'CVC',
+        'OMG'
+    ];
+
+    const assets = await getAssets();
+
+    const myAssets = assets.filter(asset => myCoins.includes(asset.asset_id));
+
+    return myAssets;
+}
+
+const getPrices = async (assets: Asset[]): Promise<Array<Price>> => {
+    
+    const prices = assets.map((asset) => ({
+        symbol: asset.asset_id,
+        usd: asset.price_usd
+    }));
+
+    return prices;
+};
+
+(async () => {
+
+    const assets = await getMyAssets();
+
+    const prices = getPrices(assets);
+
+    console.log(prices);
+
 })();
--- types.ts
+++ types.ts
@@ -1,9 +1,24 @@
-interface IPriceData {
-    base: string;
-    currency: string;
-    amount: string;
+export interface Price {
+    symbol: string;
+    usd: number;
 }
 
-export interface IPrice {
-    data: IPriceData;
+export interface Asset {
+    asset_id: string;
+    name: string;
+    type_is_crypto: number;
+    data_start: string;
+    data_end: string;
+    data_quote_start: string;
+    data_quote_end: string;
+    data_orderbook_start: string;
+    data_orderbook_end: string;
+    data_trade_start: string;
+    data_trade_end: string;
+    data_symbols_count: number;
+    price_usd: number;
+    volume_1hrs_usd: number;
+    volume_1day_usd: number;
+    volume_1mth_usd: number;
+    id_icon: string;
 }
